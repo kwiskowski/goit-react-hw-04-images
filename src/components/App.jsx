13ce9b7883c54314b93d8f4c03974e5810e873ef
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2024-02-05 23:11:01 +0100

    finally working
    code refactor using hooks and bugfixing (error 400)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,140 +1,67 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { SearchBar } from './SearchBar/SearchBar';
-import { imagesFetch } from './ImageFetch/ImageFetch';
 import { ToastContainer, toast, Flip } from 'react-toastify';
+import { imagesFetch } from './ImageFetch/ImageFetch';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-  // state = {
-  //   images: [],
-  //   isLoading: false,
-  //   error: null,
-  //   query: '',
-  //   page: 1,
-  //   showModal: false,
-  //   selectedImage: null,
-  //   isLastPage: false,
-  // };
-  const [query, setQuery] = useState('');
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
-  const [showLoader, setShowLoader] = useState(false);
-  const [largeImageUrlAndTags, setLargeImageUrlAndTags] = useState(null);
-  const [totalImages, setTotalImages] = useState(null);
-
-  // const [images, setImages] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
-  // const [query, setQuery] = useState('');
-  // const [page, setPage] = useState(1);
-  // const [showModal, setShowModal] = useState(false);
-  // const [selectedImage, setSelectedImage] = useState(null);
-  // const [isLastPage, setIsLastPage] = useState(false);
-
-  // componentDidUpdate(_prevProps, prevState) {
-  //   if (prevState.query !== this.state.query) {
-  //     this.setState({ images: [], page: 1, isLastPage: false }, () => {
-  //       this.fetchImages();
-  //     });
-  //   }
-  // }
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const [totalPics, setTotalPics] = useState(null);
 
   useEffect(() => {
-    if (!query) {
-      // fetchImages();
-      return;
-    }
+    if (!query) return;
 
-    imagesFetch(query, page)
-      .then(dataImages => {
-        setImages(prevImages => [...prevImages, ...dataImages.hits]);
-        setShowLoader(false);
-        setTotalImages(dataImages.totalHits);
-      })
-      .catch(error => {
-        console.log(error);
-        setShowLoader(false);
-      });
+    const fetchImages = async () => {
+      setIsLoading(true);
+      try {
+        const { hits, totalHits } = await imagesFetch(query, page);
+        setImages(prevImages => (page === 1 ? hits : [...prevImages, ...hits]));
+        setTotalPics(totalHits);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchImages();
   }, [query, page]);
 
-  //   fetchImages()  {
-  //     const API_KEY = '39429562-362aa611c83bf0adbf53209b3';
-
-  //     setIsLoading(true);
-
-  //     axios
-  //       .get(
-  //         `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-  //       )
-  //       .then(response => {
-  //         const { hits, totalHits } = response.data;
-
-  //         if (hits.length === 0) {
-  //           return toast(
-  //             'Sorry, there are no images matching your request...',
-  //             {
-  //               position: toast.POSITION.TOP_CENTER,
-  //               icon: '🤔',
-  //             }
-  //           );
-  //         }
-
-  //         const modifiedHits = hits.map(
-  //           ({ id, tags, webformatURL, largeImageURL }) => ({
-  //             id,
-  //             tags,
-  //             webformatURL,
-  //             largeImageURL,
-  //           })
-  //         );
-
-  //         this.setState(prevState => ({
-  //           images: [...prevState.images, ...modifiedHits],
-  //           page: prevState.page + 1,
-  //           isLastPage:
-  //             prevState.images.length + modifiedHits.length >= totalHits,
-  //         }));
-  //       })
-
-  //       .catch(error => {
-  //         setError(error.message);
-  //       })
-  //       .finally(() => {
-  //         setIsLoading(false);
-  //       });
-  //   };
-  // }, [query, page]);
-
   const handleSearchSubmit = newQuery => {
-    if (query === newQuery) {
+    if (newQuery === query) {
       return;
     }
-
     setQuery(newQuery);
-    setImages([]);
     setPage(1);
-    setShowLoader(true);
-
-    window.scrollTo({ top: 0, left: 0 });
+    setImages([]);
+    setError(null);
+    setIsLastPage(false);
   };
 
   const handleImageClick = image => {
-    // this.setState({ selectedImage: image, showModal: true });
     setSelectedImage(image);
     setShowModal(true);
     document.body.style.overflow = 'hidden';
   };
 
   const handleModalClose = () => {
-    // this.setState({ selectedImage: null, showModal: false });
     setSelectedImage(null);
     setShowModal(false);
     document.body.style.overflow = 'auto';
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
   };
 
   return (
@@ -158,7 +85,7 @@ export const App = () => {
       {isLoading && <Loader />}
 
       {!isLoading && images.length > 0 && !isLastPage && (
-        <Button onClick={imagesFetch} />
+        <Button onClick={handleLoadMore} />
       )}
 
       {showModal && <Modal image={selectedImage} onClose={handleModalClose} />}
--- src/components/ImageFetch/ImageFetch.jsx
+++ src/components/ImageFetch/ImageFetch.jsx
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
-export const imagesFetch = async (query, page) => {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const KEY = '39429562-362aa611c83bf0adbf53209b3';
-  const url = `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`;
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function imagesFetch(query, page) {
+  const options = {
+    params: {
+      key: '39429562-362aa611c83bf0adbf53209b3',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: page,
+      per_page: 12,
+    },
+  };
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(BASE_URL, options);
     return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+  } catch (error) {}
+}
